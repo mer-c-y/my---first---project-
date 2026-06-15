3e9119183622a93bbc6b61edccfd8701e38fbc2f
mer-c-y <africanbae3@gmail.com>
2026-06-15 20:32:57 +0300

    Add initial JavaScript program in Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,10 @@
+// My first JavaScript program!
+// JavaScript runs in the web browser
+
+const name = "Mercy";
+const message = `Hello, ${name}! This is my first JavaScript code!`;
+
+console.log("I'm learning to code on GitHub");
+
+// Display in HTML
+document.write("<h1>" + message + "</h1>");
